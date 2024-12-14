401a5029b279347052f05c4f93c23231c00a63d2
Krits-devops <kritagyarm9@gmail.com>
2024-12-14 18:14:25 -0500

    Components Added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { apiUrl,filterData } from "./data";
+import Navbar from "./components/Navbar";
+import Filter from "./components/Filter";
+import Cards from "./components/Cards";
 
 const App = () => {
-  return <div>App</div>;
+
+
+  return (
+
+    <div>
+      <Navbar/>
+
+      <Filter
+          filterData = {filterData}
+      />
+
+      <Cards/>
+
+    </div>
+
+  );
 };
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+
+
+const Navabar = ()=>{
+    return(
+        <div>
+            <nav>
+                <h1>Top Courses</h1>
+            </nav>
+        </div>
+    )
+}
+
+export default Navabar;
