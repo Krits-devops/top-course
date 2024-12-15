3bcf9b0a05a411fa1f165fd3ef2ddd2637579fa8
Krits-devops <kritagyarm9@gmail.com>
2024-12-14 23:53:22 -0500

    API data fetching
    Added API data fetching and rendering to App.js and Cards.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { apiUrl,filterData } from "./data";
 import Navbar from "./components/Navbar";
 import Filter from "./components/Filter";
 import Cards from "./components/Cards";
+import { toast } from "react-toastify";
 
 const App = () => {
 
+  const [courses, setCourses] = useState(null);
+
+  useEffect(()=>{
+    const fetchData = async()=>{
+      try{
+          const res = await fetch(apiUrl);
+          const output = await res.json();
+          console.log(output);
+          setCourses(output.data);
+      }
+      catch(error){
+        toast.error("Something Went Wrong");
+      }
+    }
+    fetchData();
+  },[]);
 
   return (
 
@@ -16,7 +33,7 @@ const App = () => {
           filterData = {filterData}
       />
 
-      <Cards/>
+      <Cards courses={courses}/>
 
     </div>
 
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,28 @@
+import React from "react";
+import Card from "./Card";
 
-const Cards = ()=>{
-    return
+const Cards = ({courses})=>{
+
+    // It return all courses information recieved from api response.
+     let allCourses=[];
+    const getCourses = ()=>{
+        Object.values(courses).forEach((courseCategory)=>{
+            courseCategory.forEach((course)=>{
+                allCourses.push(course);
+        })
+    })
+    return allCourses;
+   
+}   
+    return(
+        <div>
+            {
+            getCourses().map((course)=>{
+                <Card/>
+            })
+        }
+        </div>
+    )
 }
 
 export default Cards;
